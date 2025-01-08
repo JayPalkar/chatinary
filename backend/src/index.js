abb07e9e949f11efe0e77f546078f3fbb88b3d50
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-08 07:24:44 +0530

    authentication Routes created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import authenticationRoutes from "./routes/authentication.routes.js";
 
 const app = express();
+
+app.use("/api/auth", authenticationRoutes);
 
 app.listen(8000, () => {
   console.log("server is running on port 8000");
