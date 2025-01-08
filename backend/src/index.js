76f6528ea7dd19532a93b6355550aee56ac1bca0
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-08 07:42:27 +0530

    completed mongoDB databse setup for the project (#3)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
 import authenticationRoutes from "./routes/authentication.routes.js";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authenticationRoutes);
 
-app.listen(8000, () => {
-  console.log("server is running on port 8000");
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
 });
