57f56321e9fe7a65203bc7c2314652a12c5d2037
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-09 10:36:09 +0530

    profile-update api completed (#9)
    * middleWare for authentication created
    
    * configured cloudinary
    
    * profile-update api completed

--- backend/src/controllers/authentication.controller.js
+++ backend/src/controllers/authentication.controller.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -89,6 +90,30 @@ export const logout = (req, res) => {
     });
   } catch (error) {
     console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePicture } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePicture) {
+      return res.status(400).json({ message: "Profile picture not provided" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePicture);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePicture: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update Profile", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
