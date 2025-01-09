fe2bdb6f2a9d17421c290a0b46142d2089055649
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-09 10:06:02 +0530

    middleWare for authentication created

--- backend/src/routes/authentication.routes.js
+++ backend/src/routes/authentication.routes.js
@@ -3,14 +3,16 @@ import {
   login,
   logout,
   signup,
+  updateProfile,
 } from "../controllers/authentication.controller.js";
+import { protectRoute } from "../middleware/authentication.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
