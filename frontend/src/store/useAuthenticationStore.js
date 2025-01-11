a1854aa0b5c4d46c02083ce324afe32b4eab357b
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-11 17:27:32 +0530

    signup function created

--- frontend/src/store/useAuthenticationStore.js
+++ frontend/src/store/useAuthenticationStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthenticationStore = create((set) => ({
   authUser: null,
@@ -17,6 +18,20 @@ export const useAuthenticationStore = create((set) => ({
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
     }
   },
 }));
