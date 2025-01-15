fc59aa275bbeaaece980021c4a8208acb2e36214
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-15 18:48:41 +0530

    added default profile picture

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: "",
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
   {
