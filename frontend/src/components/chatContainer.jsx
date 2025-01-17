f45ef981bda18e7dac1f98f4284f7cd29a00ff6a
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-17 10:48:55 +0530

    created chat container layout

--- frontend/src/components/chatContainer.jsx
+++ frontend/src/components/chatContainer.jsx
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./chatHeader";
+import MessageInput from "./messageInput";
+
 const ChatContainer = () => {
-  return <div>Chat container</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+  if (isMessagesLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>Messages ...</p>
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
