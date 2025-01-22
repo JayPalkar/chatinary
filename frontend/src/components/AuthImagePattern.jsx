8b2f90244c4bf3f893a8d34e431e4c659f82dcfd
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-01-22 18:03:54 +0530

    fixed some UI

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,20 +1,15 @@
+import authorizationImage from "../asset/authorizationImage.png";
+
 /* eslint-disable react/prop-types */
 const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
-        <div className="grid grid-cols-3 gap-3  mb-8">
-          {[...Array(9)].map((_, i) => (
-            <div
-              key={i}
-              className={`aspect-square rounded-full bg-primary  ${
-                i % 2 === 0 ? "animate-pulse" : "animate-pulse"
-              } `}
-            />
-          ))}
+        <div>
+          <img src={authorizationImage} className=" animate-pulse" alt="" />
+          <h2 className="text-2xl font-bold mb-4">{title}</h2>
+          <p className="text-base-content/60">{subtitle}</p>
         </div>
-        <h2 className="text-2xl font-bold mb-4">{title}</h2>
-        <p className="text-base-content/60">{subtitle}</p>
       </div>
     </div>
   );
