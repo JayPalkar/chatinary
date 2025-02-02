bea2b8b1d767f20fe0482470a25867a81174528a
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-02-02 18:52:36 +0530

    fixed ui bug to keep up with the streak

--- frontend/src/components/sideBar.jsx
+++ frontend/src/components/sideBar.jsx
@@ -70,7 +70,7 @@ const SideBar = () => {
                 {onlineUsers.includes(chat._id) && (
                   <span
                     className="absolute bottom-0 right-0 size-3 bg-green-500 
-                  rounded-full ring-2 ring-zinc-900"
+                  rounded-full ring-2 ring-base-300"
                   />
                 )}
               </div>
