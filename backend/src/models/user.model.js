46f8cbf0818d8e83a2175caa0a27226f31fcd64f
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-09-19 13:22:59 +0530

    changed avatar url

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -17,8 +17,7 @@ const userSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+      default: "https://avatar.iran.liara.run/public",
     },
     about: {
       type: String,
